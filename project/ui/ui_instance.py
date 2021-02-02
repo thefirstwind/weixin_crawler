622d96cf60ee6b37dbf5fed6614b7a24bc9ed751
xnxing <xnxing@moreecare.com>
2021-02-02 19:23:25 +0800

    first commit

--- project/ui/ui_instance.py
+++ project/ui/ui_instance.py
@@ -19,17 +19,20 @@ class CustomFlask(Flask):
 app = CustomFlask('WeixinCrawler',template_folder="./ui/templates",static_folder="./ui/static")
 # app = CustomFlask('WeixinCrawler',template_folder="./templates",static_folder="./static")
 app.config['SECRET_KEY'] = 'secret!'
-app.config['origin'] = "http://localhost:5000"
-app.config['Access-Control-Allow-Origin'] = "http://localhost:5000"
-app.config['Access-Control-Allow-Credentials'] = True
-app.config['Access-Control-Allow-Headers'] ='Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization'
-app.config['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
+
+CORS(app)
+
+# app.config['origin'] = "http://localhost:5000"
+# app.config['Access-Control-Allow-Origin'] = "http://localhost:5000"
+# app.config['Access-Control-Allow-Credentials'] = True
+# app.config['Access-Control-Allow-Headers'] ='Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization'
+# app.config['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
 # app.config['CORS_AUTOMATIC_OPTIONS'] = True
 # app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
 
 # socketio = SocketIO(app)
 # socketio = SocketIO(app, async_mode='gevent')
-socketio = SocketIO(app, async_mode='gevent',cors_allowed_origins="*")
+socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
 
 the_redis = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
